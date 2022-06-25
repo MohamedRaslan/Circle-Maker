e6a6f9a7a3b4b8e85a47b2559fa84b3518d19bc8
MohamedRaslan <38933052+MohamedRaslan@users.noreply.github.com>
2022-06-25 14:17:19 +0200

    fix ci pipline issue

--- tests/test_circlemaker_range.py
+++ tests/test_circlemaker_range.py
@@ -20,6 +20,7 @@ def capture(command):
         command,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
+        shell=True
     )
     out, err = proc.communicate()
     return out, err, proc.returncode
